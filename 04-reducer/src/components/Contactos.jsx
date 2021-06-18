e12b3287e4cd49ac3d6343a79f70833da976be6f
Sebas995 <semarva995@hotmail.com>
2021-06-17 20:54:38 -0500

    JS - Finalizacion de app de contactos react

--- 04-reducer/src/components/Contactos.jsx
+++ 04-reducer/src/components/Contactos.jsx
@@ -1,15 +1,32 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect, useState } from 'react';
 import { ContactosReducer } from '../reducers/ContactosReducer';
 import FormularioAdd from './FormularioAdd';
 import TablaContactos from './TablaContactos';
 
+const init = () => {
+    const contactos = localStorage.getItem("contactos");
+
+    return contactos != null ? JSON.parse(contactos) : [];
+};
+
 const Contactos = () => {
 
-    const [state, dispatch] = useReducer(ContactosReducer, []);
+    const [state, dispatch] = useReducer(ContactosReducer, [], init);
+    const [formView, setformView] = useState(false);
+
+    useEffect(() => {
+        localStorage.setItem("contactos", JSON.stringify(state));
+    }, [state]);
 
     return (
         <div className="container mt-3">
-            <FormularioAdd dispatch={ dispatch }/>
+
+            <button className="btn btn-success" onClick={ () => setformView(!formView) }>{ formView ? "-" : "+" } Agregar Contacto</button>
+
+            {
+                formView && <FormularioAdd dispatch={ dispatch }/> 
+            }
+            
             <TablaContactos contactos={ state } dispatch={ dispatch }/>
         </div>
     )
